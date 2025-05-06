c18e7f1c4b5b29793fd7aa3ba967c8286dc8e425
MawlaBarvez233510836 <mawlabarvez@student.uir.ac.id>
2025-05-06 20:03:53 +0800

    user dapat melihat kegiatan yang sudah ditambahkan di aplikasi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Daftar Kegiatan</h1>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task.text }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        { text: "Belajar ", completed: false },
+        { text: "makan", completed: false }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: auto;
+  padding: 20px;
 }
 </style>
